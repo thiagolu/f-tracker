bb63226d918574e5ccd37110fc4826f12e0dce0c
Thiago Lu <tlusilva05@gmail.com>
2016-07-26 20:25:45 -0300

    add description to bills

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
 	def hello
 
     @bill_price = Bill.calculate(:sum, :price)
+    @bill_description = []
+    bill_description
 	end
 
 	def my_portifolio
@@ -10,4 +12,12 @@ class PagesController < ApplicationController
 	end
 
 	private
+
+  def bill_description
+    Bill.all.each do |bill|
+      @bill_description << bill.description
+    end
+    @bill_description
+  end
+
 end
